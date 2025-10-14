8bf1c4f83f300b782df8a719cc2e48a13de89d9d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-10-14 20:04:52 +0100

    removed packages dir from root project

--- packages/ai-core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""AI Core Package for Brandalyze"""
-
-from .ai_core.embeddings import generate_embedding, store_embedding
-from .ai_core.analysis import BrandAnalyzer
-from .ai_core.prompts import get_analysis_prompt
